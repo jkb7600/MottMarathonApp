3e2e842693fa9f032c23a4aa2dc612f45ef6a964
jkb7600 <Justin@Justins-MacBook-Pro.local>
2014-01-25 00:57:12 -0500

    initial LapsTableViewController commit

---
+++ MottMarathonApp/LapsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LapsTableViewController.h
+//  MottMarathonApp
+//
+//  Created by Justin Bennett on 1/25/14.
+//  Copyright (c) 2014 jkb7600. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LapsTableViewController : UITableViewController
+@property (weak, nonatomic) NSArray *laps;
+@end
